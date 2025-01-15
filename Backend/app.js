ac60db1a0562939622914b0121cf4ee2c5714642
anurag4me <anuragmishra5033@gmail.com>
2025-01-15 08:44:33 +0530

    modified basic server by using dotenv

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,16 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
+// middlewares
+app.use(cors());
+
+// routes
 app.get("/", (req, res) => {
-  return res.json({ test: "Successful" });
+  return res.send("Hello World!");
 });
 
-app.listen(PORT, () =>
-  console.log(`Server Started at http://localhost:${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
