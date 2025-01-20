09ad9f2d8cd84097fe0bdd55d8a71680f2d89f87
anurag4me <anuragmishra5033@gmail.com>
2025-01-20 09:13:25 +0530

    Updated user protector wrapper

--- Frontend/src/pages/UserProtectWrapper.jsx
+++ Frontend/src/pages/UserProtectWrapper.jsx
@@ -1,16 +1,41 @@
-import React, { useEffect } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useUser } from "../context/UserContext";
 
 const UserProtectWrapper = ({ children }) => {
   const navigate = useNavigate();
-
+  const [loading, setLoading] = useState(true);
   const token = localStorage.getItem("token");
+  const { setUser } = useUser();
 
   useEffect(() => {
     if (!token) {
       navigate("/login");
     }
-  }, [token]);
+
+    axios
+      .get(`${import.meta.env.VITE_BASE_URL}/users/profile`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setUser(response.data.user);
+          setLoading(false);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        localStorage.removeItem("token");
+        navigate("/login");
+      });
+  }, [token, navigate, setUser]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return <>{children}</>;
 };
